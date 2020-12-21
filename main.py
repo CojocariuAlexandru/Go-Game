a962607bfc45dc2776d92585ba5cedd50b346c69
CojocariuAlexandru <53466892+CojocariuAlexandru@users.noreply.github.com>
2020-12-21 18:23:57 +0200

    refactoring, computer and player options button added

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from Scripts.Utility import utilityFunctions as util
 from Scripts.Home import home
 from Scripts.Game import game
 
+
+
 def adjust_window(length, width):
     global window
     window = GraphWin("Go Game", length, width)
@@ -11,22 +13,27 @@ def start_game():
     global window
     game_run_flag = True
     game_state = "Home"
+    home_manager = home.HomeManager()
+    game_manager = game.GameManager()
 
     while game_run_flag == True:
         if game_state == "Home":
             game_drawn_flag = False
-            home.draw_home(window)
-            (game_run_flag, game_state) = home.handle_mouse_click_home(window)
+            home_manager.draw_home(window)
+            (game_run_flag, game_state) = home_manager.handle_mouse_click_home(window)
+        elif game_state == "Pressed play":
+            game_drawn_flag = False
+            (game_run_flag, game_state) = home_manager.handle_mouse_click_home(window)
         elif game_state == "Game":
             if game_drawn_flag == False:
                 turn = 'black'
-                game.draw_game(window, turn)
+                game_manager.draw_game(window, turn)
                 game_drawn_flag = True
 
             piece = Circle(Point(810, 200), 20)
             piece.setFill(turn)
             piece.draw(window)
-            (game_run_flag, game_state) = game.handle_mouse_click_game(window, turn)
+            (game_run_flag, game_state) = game_manager.handle_mouse_click_game(window, turn)
 
             if game_state == 'PutPiece':
                 game_state = 'Game'
